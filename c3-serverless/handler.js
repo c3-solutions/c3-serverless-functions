34418087d15d8e2615f082a56e5765ee8d470a73
Wes Coderre <wesley.coderre@gmail.com>
2019-07-22 16:58:36 -0700

    serverless funtion deployed and working

--- c3-serverless/handler.js
+++ c3-serverless/handler.js
@@ -1,18 +1,17 @@
 'use strict';
 
+const axios = require('axios')
+const conf = require('./config.js')
+
 module.exports.hello = async event => {
+  const response = await axios({
+    method: 'get',
+    url: 'https://api.github.com/orgs/c3-solutions/members',
+    headers: {'Authorization': `token ${conf.PAT}`}
+  });
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify(response.data),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
