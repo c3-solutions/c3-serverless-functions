8304894d49d1154578bbfc7e6276320484ba54ef
Wes Coderre <wesley.coderre@gmail.com>
2019-07-22 19:26:45 -0700

    refactored data

--- c3-serverless/handler.js
+++ c3-serverless/handler.js
@@ -4,11 +4,23 @@ const axios = require('axios')
 const conf = require('./config.js')
 
 module.exports.hello = async event => {
+
+  const users = []
+
   const response = await axios({
     method: 'get',
     url: 'https://api.github.com/orgs/c3-solutions/members',
     headers: {'Authorization': `token ${conf.PAT}`}
   });
+
+  const rebuild = await Promise.all(response.data.map(async (member) => {
+    const userData = await axios({
+    method: 'get',
+    url: member.url,
+    headers: {'Authorization': `token ${conf.PAT}`}
+    });
+    return userData.data;
+  }))
 
   return {
     statusCode: 200,
@@ -16,6 +28,6 @@ module.exports.hello = async event => {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     },
-    body: JSON.stringify(response.data),
+    body: JSON.stringify(rebuild),
   };
 };
